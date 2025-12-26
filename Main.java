93826e6f2c2f15b051afedace4827b9226956796
niralanitish <nnirala110@gmail.com>
2025-12-26 19:32:53 +0530

    new file

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+       System.out.println("You entered: " + n);
+        scanner.close();
+    }
+}
